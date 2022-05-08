d98074ed2ce7919401fe53cc9a85ec6cf9c2d6f5
Natasha Vitorino <seu-email-cadastrado-no-git>
2022-05-08 13:07:08 -0300

    first commit

---
+++ TCC_Tela Inicial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCC_Tela_Inicial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
